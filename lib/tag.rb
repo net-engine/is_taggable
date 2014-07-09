353edd321491e1128e554efefd231dca2f11b9cd
Lucas Caton <lucascaton@gmail.com>
2014-07-09 13:38:29 +1000

    Making it compatible with Rails 4.1.x

--- lib/tag.rb
+++ lib/tag.rb
@@ -7,9 +7,9 @@ class Tag < ActiveRecord::Base
 
   has_many :taggings, dependent: :destroy
 
-  validates_presence_of :name
+  validates_presence_of   :name
   validates_uniqueness_of :name, scope: :kind
 
-  named_scope :with_name_like_and_kind, lambda { |name, kind| { conditions: ['name like ? AND kind = ?', name, kind] } }
-  named_scope :of_kind,                 lambda { |kind| { conditions: { kind: kind } } }
+  scope :with_name_like_and_kind, -> (name, kind) { where(['name like ? AND kind = ?', name, kind]) }
+  scope :of_kind,                 -> (kind)       { where(kind: kind) }
 end
