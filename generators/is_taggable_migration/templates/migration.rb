673520c8cf733e99b635e29543458d1f5d6af80d
Lucas Caton <lucascaton@gmail.com>
2014-07-09 13:33:54 +1000

    Cleaning the code

--- generators/is_taggable_migration/templates/migration.rb
+++ generators/is_taggable_migration/templates/migration.rb
@@ -7,8 +7,7 @@ class IsTaggableMigration < ActiveRecord::Migration
 
     create_table :taggings do |t|
       t.integer :tag_id
-
-      t.string :taggable_type, default: ''
+      t.string  :taggable_type, default: ''
       t.integer :taggable_id
     end
 
